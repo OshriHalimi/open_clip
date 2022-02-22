212c403663e9b0c8368b219f4e4717d83c901dec
Admin Pass Is 123 <oshri.halimi@gmail.com>
2022-02-22 17:06:03 +0200

    sbatch script

---
+++ scripts/train_clip.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ~/ibm/open_clip
+
+export PYTHONPATH=./src
+
+export PATH=/home/oshri.halimi/anaconda3/bin:$PATH
+
+python src/training/main.py \
+--save-frequency \
+1 \
+--zeroshot-frequency \
+1 \
+--report-to \
+tensorboard \
+--train-data="~/ibm/Data/CC3M/Train_GCC-training_output.csv" \
+--val-data="~/ibm/Data/CC3M/Validation_GCC-1.1.0-Validation_output.csv" \
+--imagenet-val=~/ibm/Data/ImageNet/ILSVRC2012_img_val \
+--csv-img-key \
+filepath \
+--csv-caption-key \
+title \
+--warmup \
+10000 \
+--batch-size=256 \
+--lr=1e-3 \
+--wd=0.1 \
+--epochs=30 \
+--workers=8 \
+--model \
+RN50 \
+--ngpus_per_node 8 \
+--debug --multigpu 0,1,2,3,4,5,6,7 \
